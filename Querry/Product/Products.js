54b0302c7967587ac87543cf0533683541f9b0f8
Rat <ratulbhowmick65@gmail.com>
2021-08-12 00:32:23 +0600

    Updated Product Querry

--- Querry/Product/Products.js
+++ Querry/Product/Products.js
@@ -3,136 +3,116 @@ const Utils = require("../../Utils/Utils");
 
 const LIMIT_THRESHOLD = 15;
 const Products = {
-	addProducts,
-	addProductToCategories,
-	addProductGallery,
-	getProducts,
-	getFeaturedProducts,
-	getPopularProducts,
-	addProductDetails,
-	addProductToAttributes,
-	addProductToVariants,
-	getSingleProductDetails,
-	getProductVariants
+  addProducts,
+  addProductToCategories,
+  addProductGallery,
+  getProducts,
+  getFeaturedProducts,
+  getPopularProducts,
+  addProductDetails,
+  addProductToAttributes,
+  addProductToVariants,
+  getSingleProductDetails,
+  getProductVariants,
 };
 
 async function addProducts(productDetails) {
-	const sqlInsert =
-		"Insert Into product (sku,parent_id,product_status_id, productType, view_on_website,featured_product,popular_product,isTaxable,hasFreeShipping,isDisableDiscount,manageStock,inventory_status,featured_img,regular_price,discount_price, inserted_at, updated_at) Values ?";
+  const sqlInsert =
+    "Insert Into product (sku,parent_id,product_status_id, productType, view_on_website,featured_product,popular_product,isTaxable,hasFreeShipping,isDisableDiscount,manageStock,inventory_status,featured_img,regular_price,discount_price, inserted_at, updated_at) Values ?";
 
-	return PromiseModule.createUpdateDelete(sqlInsert, productDetails);
+  return PromiseModule.createUpdateDelete(sqlInsert, productDetails);
 }
 
 async function addProductDetails(productDetails) {
-	const {
-		product_id,
-		product_name,
-		short_description,
-		long_description,
-		product_gallery,
-		inserted_at,
-		updated_at
-	} = productDetails;
+  const sqlInsert =
+    "Insert into product_details (product_id, product_name,short_description,long_description,product_gallery,inserted_at,updated_at) Values ?";
 
-	const sqlInsert =
-		"Insert into product_details (product_id, product_name,short_description,long_description,product_gallery,inserted_at,updated_at) Values (?,?,?,?,?,?,?)";
-
-	const productDetailsData = [
-		product_id,
-		product_name,
-		short_description,
-		long_description,
-		product_gallery,
-		inserted_at,
-		updated_at
-	];
-
-	return PromiseModule.createUpdateDelete(sqlInsert, productDetailsData);
+  return PromiseModule.createUpdateDelete(sqlInsert, [productDetails]);
 }
 
 async function addProductToCategories(categories) {
-	const sqlInsert =
-		"Insert into product_categories(category_id,product_id,inserted_at,	updated_at) Values ?";
+  const sqlInsert =
+    "Insert into product_categories(category_id,product_id,inserted_at,	updated_at) Values ?";
 
-	return PromiseModule.createUpdateDelete(sqlInsert, categories);
+  return PromiseModule.createUpdateDelete(sqlInsert, categories);
 }
 
 async function addProductToAttributes(attributes) {
-	const sqlInsert =
-		"Insert into product_attribute(product_id,attribute_id) Values ?";
-	return PromiseModule.createUpdateDelete(sqlInsert, attributes);
+  const sqlInsert =
+    "Insert into product_attribute(product_id,attribute_id) Values ?";
+  return PromiseModule.createUpdateDelete(sqlInsert, attributes);
 }
 
 async function addProductToVariants(product_variants) {
-	const sqlInsert =
-		"Insert into product_variants(product_variant_id,product_variant_combinations) Values ?";
+  const sqlInsert =
+    "Insert into product_variants(product_variant_id,product_variant_combinations) Values ?";
 
-	return PromiseModule.createUpdateDelete(sqlInsert, product_variants);
+  return PromiseModule.createUpdateDelete(sqlInsert, product_variants);
 }
 
 async function addProductGallery(product_gallery) {
-	const sqlInsert =
-		"Insert Into product_details (product_id,product_gallery) values (?,?)";
+  const sqlInsert =
+    "Insert Into product_details (product_id,product_gallery) values (?,?)";
 
-	return PromiseModule.createUpdateDelete(sqlInsert, product_gallery);
+  return PromiseModule.createUpdateDelete(sqlInsert, product_gallery);
 }
 
 async function getProducts(pageNumber, productLimit) {
-	let sqlSearch =
-		"SELECT p.product_id,p.isTaxable,p.isDisableDiscount,p.hasFreeShipping,p.featured_img,p.regular_price,p.discount_price,pd.product_name,Product_attribute.attribute_id,Product_attribute.option_name, Product_attribute.option_id, Product_attribute.attribute_name FROM product as p INNER JOIN product_details as pd ON p.product_id = pd.product_id LEFT JOIN ( SELECT pa.attribute_id, pa.product_id, attributes.attribute_name,options.option_id,options.option_name FROM product_attribute as pa, attributes,options WHERE pa.attribute_id = attributes.attribute_id AND options.attribute_id = attributes.attribute_id ) as Product_attribute On Product_attribute.product_id = p.product_id ORDER BY p.product_id";
+  let sqlSearch =
+    "SELECT p.product_id,p.isTaxable,p.isDisableDiscount,p.hasFreeShipping,p.featured_img,p.regular_price,p.discount_price,pd.product_name,Product_attribute.attribute_id,Product_attribute.option_name, Product_attribute.option_id, Product_attribute.attribute_name FROM product as p INNER JOIN product_details as pd ON p.product_id = pd.product_id LEFT JOIN ( SELECT pa.attribute_id, pa.product_id, attributes.attribute_name,options.option_id,options.option_name FROM product_attribute as pa, attributes,options WHERE pa.attribute_id = attributes.attribute_id AND options.attribute_id = attributes.attribute_id ) as Product_attribute On Product_attribute.product_id = p.product_id ORDER BY p.product_id";
 
-	if (pageNumber) {
-		// limit
-		const page = parseInt(pageNumber);
-		const limit = productLimit ? parseInt(productLimit) : LIMIT_THRESHOLD;
-		const offset = (page - 1) * limit;
-		sqlSearch = `${sqlSearch} LIMIT  ${limit} OFFSET  ${offset}`;
-	}
-	return PromiseModule.readData(sqlSearch);
+  if (pageNumber) {
+    // limit
+    const page = parseInt(pageNumber);
+    const limit = productLimit ? parseInt(productLimit) : LIMIT_THRESHOLD;
+    const offset = (page - 1) * limit;
+    sqlSearch = `${sqlSearch} LIMIT  ${limit} OFFSET  ${offset}`;
+  }
+  return PromiseModule.readData(sqlSearch);
 }
 
 // SELECT * FROM product,product_variants,prduct_inventory WHERE product_variants.product_variant_id = product.product_id And product.parent_id = 128 And prduct_inventory.product_id = product.product_id
 
 async function getSingleProductDetails(product_id) {
-	const sqlSearch = `SELECT * FROM product INNER JOIN product_details ON product.product_id = product_details.product_id  AND product.product_id = ${product_id} LEFT JOIN ( SELECT product_attribute.attribute_id, product_attribute.product_id, attributes.attribute_name,options.option_id,options.option_name FROM product_attribute, attributes,options WHERE product_attribute.attribute_id = attributes.attribute_id AND options.attribute_id = attributes.attribute_id ) as Product_attribute On Product_attribute.product_id = product.product_id LEFT JOIN prduct_inventory ON prduct_inventory.product_id = product.product_id ORDER BY product.product_id`;
+  const sqlSearch = `SELECT * FROM product INNER JOIN product_details ON product.product_id = product_details.product_id  AND product.product_id = ${product_id} LEFT JOIN ( SELECT product_attribute.attribute_id, product_attribute.product_id, attributes.attribute_name,options.option_id,options.option_name FROM product_attribute, attributes,options WHERE product_attribute.attribute_id = attributes.attribute_id AND options.attribute_id = attributes.attribute_id ) as Product_attribute On Product_attribute.product_id = product.product_id LEFT JOIN prduct_inventory ON prduct_inventory.product_id = product.product_id ORDER BY product.product_id`;
 
-	return PromiseModule.readData(sqlSearch);
+  return PromiseModule.readData(sqlSearch);
 }
 
 async function getProductVariants(product_id) {
-	const sqlSearch = `SELECT * FROM product,product_variants,prduct_inventory WHERE product_variants.product_variant_id = product.product_id And product.parent_id = ${product_id} And prduct_inventory.product_id = product.product_id`;
+  const sqlSearch = `SELECT * FROM product,product_variants,prduct_inventory WHERE product_variants.product_variant_id = product.product_id And product.parent_id = ${product_id} And prduct_inventory.product_id = product.product_id`;
 
-	return PromiseModule.readData(sqlSearch);
+  return PromiseModule.readData(sqlSearch);
 }
 
 async function getFeaturedProducts(type, pageNumber, productLimit) {
-	let sqlSearch = "";
+  let sqlSearch = "";
 
-	if (pageNumber) {
-		// limit
-		const page = parseInt(pageNumber);
-		const limit = productLimit ? parseInt(productLimit) : LIMIT_THRESHOLD;
-		const offset = (page - 1) * limit;
-		sqlSearch = `Select * from product where featured_product = ${type} LIMIT  ${limit} OFFSET  ${offset}`;
-	} else {
-		sqlSearch = `Select * from product where featured_product = ${type}`;
-	}
-	return PromiseModule.readData(sqlSearch);
+  if (pageNumber) {
+    // limit
+    const page = parseInt(pageNumber);
+    const limit = productLimit ? parseInt(productLimit) : LIMIT_THRESHOLD;
+    const offset = (page - 1) * limit;
+    sqlSearch = `Select * from product where featured_product = ${type} LIMIT  ${limit} OFFSET  ${offset}`;
+  } else {
+    sqlSearch = `Select * from product where featured_product = ${type}`;
+  }
+  return PromiseModule.readData(sqlSearch);
 }
 
 async function getPopularProducts(type, pageNumber, productLimit) {
-	let sqlSearch = "";
+  let sqlSearch = "";
 
-	if (pageNumber) {
-		// limit
-		const page = parseInt(pageNumber);
-		const limit = productLimit ? parseInt(productLimit) : LIMIT_THRESHOLD;
-		const offset = (page - 1) * limit;
-		sqlSearch = `Select * from product where popular_product = ${type} LIMIT  ${limit} OFFSET  ${offset}`;
-	} else {
-		sqlSearch = `Select * from product where popular_product = ${type}`;
-	}
-	return PromiseModule.readData(sqlSearch);
+  if (pageNumber) {
+    // limit
+    const page = parseInt(pageNumber);
+    const limit = productLimit ? parseInt(productLimit) : LIMIT_THRESHOLD;
+    const offset = (page - 1) * limit;
+    sqlSearch = `Select * from product where popular_product = ${type} LIMIT  ${limit} OFFSET  ${offset}`;
+  } else {
+    sqlSearch = `Select * from product where popular_product = ${type}`;
+  }
+  return PromiseModule.readData(sqlSearch);
 }
 
 module.exports = Products;
--- routes/ecommerceRoutes/productRoutes/index.js
+++ routes/ecommerceRoutes/productRoutes/index.js
@@ -5,238 +5,238 @@ const InventoryQuery = require("../../../Querry/Product/Inventory");
 const Utils = require("../../../Utils/Utils");
 
 const {
-	ProductModel,
-	ProductInventoryModel,
-	ProductResponseModel,
-	ParentProductModel
+  ProductModel,
+  ProductInventoryModel,
+  ProductResponseModel,
+  ParentProductModel,
 } = require("../../../Modles/Products");
 
 const { parentProduct } = require("../../../middleware");
 
 routes.get("/get-products", async (req, res) => {
-	const { page, limit } = req.query;
-	try {
-		const response = await ProductQuery.getProducts(page, limit);
-		const jsonData = {
-			status: "success",
-			data: {
-				total_products: response.length,
-				products: [...response]
-			}
-		};
-		res.status(200).json(jsonData);
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({ msg: "Something Went Wrong" });
-	}
+  const { page, limit } = req.query;
+  try {
+    const response = await ProductQuery.getProducts(page, limit);
+    const jsonData = {
+      status: "success",
+      data: {
+        total_products: response.length,
+        products: [...response],
+      },
+    };
+    res.status(200).json(jsonData);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "Something Went Wrong" });
+  }
 });
 
 routes.get("/get-featured-product", async (req, res) => {
-	const { page, limit } = req.query;
-	const type = req.body.type || 1;
-	try {
-		const response = await ProductQuery.getFeaturedProducts(type, page, limit);
-		const jsonData = {
-			status: "success",
-			data: {
-				total_products: response.length,
-				products: [...response]
-			}
-		};
-		res.status(200).json(jsonData);
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({ msg: "Something Went Wrong" });
-	}
+  const { page, limit } = req.query;
+  const type = req.body.type || 1;
+  try {
+    const response = await ProductQuery.getFeaturedProducts(type, page, limit);
+    const jsonData = {
+      status: "success",
+      data: {
+        total_products: response.length,
+        products: [...response],
+      },
+    };
+    res.status(200).json(jsonData);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "Something Went Wrong" });
+  }
 });
 
 routes.get("/get-popular-product", async (req, res) => {
-	/** 1 -> true
-	 *  0 -> false
-	 */
+  /** 1 -> true
+   *  0 -> false
+   */
 
-	const { page, limit } = req.query;
-	const type = req.body.type || 1;
+  const { page, limit } = req.query;
+  const type = req.body.type || 1;
 
-	try {
-		const response = await ProductQuery.getPopularProducts(type, page, limit);
-		const jsonData = {
-			status: "success",
-			data: {
-				total_products: response.length,
-				products: [...response]
-			}
-		};
-		res.status(200).json(jsonData);
-	} catch (error) {
-		console.log(error);
-		res.status(400).json({ msg: "Something Went Wrong" });
-	}
+  try {
+    const response = await ProductQuery.getPopularProducts(type, page, limit);
+    const jsonData = {
+      status: "success",
+      data: {
+        total_products: response.length,
+        products: [...response],
+      },
+    };
+    res.status(200).json(jsonData);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ msg: "Something Went Wrong" });
+  }
 });
 
 routes.get("/products", async (req, res) => {
-	const { page, limit } = req.query;
-	///here is some problem when pagination
-	try {
-		const response = await ProductQuery.getProducts(page, limit);
-		const product_data_response = [];
-		for (let i = 0; i < response.length; ) {
-			const data = response.filter((item) => {
-				return item.product_id === response[i].product_id;
-			});
-			product_data_response.push(ParentProductModel(data));
-			i = i + data.length;
-		}
-		const jsonObject = {
-			massage: "success",
-			total_products: product_data_response.length,
-			products: [...product_data_response]
-		};
-		res.status(200).json(jsonObject);
-	} catch (error) {
-		console.log(error);
-	}
+  const { page, limit } = req.query;
+  ///here is some problem when pagination
+  try {
+    const response = await ProductQuery.getProducts(page, limit);
+    const product_data_response = [];
+    for (let i = 0; i < response.length; ) {
+      const data = response.filter(item => {
+        return item.product_id === response[i].product_id;
+      });
+      product_data_response.push(ParentProductModel(data));
+      i = i + data.length;
+    }
+    const jsonObject = {
+      massage: "success",
+      total_products: product_data_response.length,
+      products: [...product_data_response],
+    };
+    res.status(200).json(jsonObject);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 routes.get("/products/:id", async (req, res) => {
-	const { id } = req.params;
+  const { id } = req.params;
 
-	try {
-		const response = await ProductQuery.getSingleProductDetails(id);
-		const parentProduct = ParentProductModel(response);
-		if (Object.entries(parentProduct).length === 0)
-			return res.status(404).json({ massage: "Product is not found" });
-		const response_of_vairants = await ProductQuery.getProductVariants(id);
+  try {
+    const response = await ProductQuery.getSingleProductDetails(id);
+    const parentProduct = ParentProductModel(response);
+    if (Object.entries(parentProduct).length === 0)
+      return res.status(404).json({ massage: "Product is not found" });
+    const response_of_vairants = await ProductQuery.getProductVariants(id);
 
-		const variants =
-			response_of_vairants.length > 0 ? [...response_of_vairants] : null;
+    const variants =
+      response_of_vairants.length > 0 ? [...response_of_vairants] : null;
 
-		parentProduct["variants"] = variants;
+    parentProduct["variants"] = variants;
 
-		const jsonObject = {
-			massage: "success",
-			product: parentProduct
-		};
+    const jsonObject = {
+      massage: "success",
+      product: parentProduct,
+    };
 
-		res.status(200).json(jsonObject);
-	} catch (error) {
-		res.status(500).json({ massage: error.massage });
-	}
+    res.status(200).json(jsonObject);
+  } catch (error) {
+    res.status(500).json({ massage: error.massage });
+  }
 });
 
 routes.post("/product", parentProduct, async (req, res) => {
-	const { product_info } = req;
-	const { inserted_at, updated_at, product_id } = product_info;
-	const {
-		variants,
-		categories,
-		product_name,
-		short_description,
-		long_description,
-		attributesList,
-		product_gallery
-	} = req.body;
+  const { product_info } = req;
+  const { inserted_at, updated_at, product_id } = product_info;
+  const {
+    variants,
+    categories,
+    product_name,
+    short_description,
+    long_description,
+    attributesList,
+    product_gallery,
+  } = req.body;
 
-	const product_details = {
-		product_id: product_id,
-		product_name: product_name,
-		short_description: short_description || null,
-		long_description: long_description || null,
-		product_gallery: product_gallery || null,
-		inserted_at: inserted_at,
-		updated_at: updated_at
-	};
+  const product_details = [
+    product_id,
+    product_name,
+    short_description || "",
+    long_description || "",
+    product_gallery || null,
+    inserted_at,
+    updated_at,
+  ];
 
-	//product and categories table data
-	const productsToCategories = categories.map((item) => [
-		item.value,
-		product_id,
-		inserted_at,
-		updated_at
-	]);
+  //product and categories table data
+  const productsToCategories = categories.map(item => [
+    item.value,
+    product_id,
+    inserted_at,
+    updated_at,
+  ]);
 
-	// product inventory data
-	const inventory = product_info.manageStock
-		? [
-				Object.values(
-					ProductInventoryModel(req.body, product_id, inserted_at, updated_at)
-				)
-		  ]
-		: null;
+  // product inventory data
+  const inventory = product_info.manageStock
+    ? [
+        Object.values(
+          ProductInventoryModel(req.body, product_id, inserted_at, updated_at)
+        ),
+      ]
+    : null;
 
-	// product and attribute table data
-	const productsToAttributes = attributesList
-		? attributesList.map((item) => [product_id, item.attributesList])
-		: null;
+  // product and attribute table data
+  const productsToAttributes = attributesList
+    ? attributesList.map(item => [product_id, item.attributesList])
+    : null;
 
-	// product variants
-	const product_variants = variants
-		? variants &&
-		  variants.map((item) =>
-				Object.values(
-					ProductModel(
-						{
-							...item,
-							parent_id: product_id
-						},
-						inserted_at,
-						updated_at
-					)
-				)
-		  )
-		: null;
+  // product variants
+  const product_variants = variants
+    ? variants &&
+      variants.map(item =>
+        Object.values(
+          ProductModel(
+            {
+              ...item,
+              parent_id: product_id,
+            },
+            inserted_at,
+            updated_at
+          )
+        )
+      )
+    : null;
 
-	try {
-		//// HAVE TO OPTIMIZE HERE
-		await ProductQuery.addProductToCategories([productsToCategories]);
-		await ProductQuery.addProductDetails(product_details);
-		productsToAttributes &&
-			(await ProductQuery.addProductToAttributes([productsToAttributes]));
-		inventory && (await InventoryQuery.addInventory([inventory]));
+  try {
+    //// HAVE TO OPTIMIZE HERE
+    await ProductQuery.addProductToCategories([productsToCategories]);
+    await ProductQuery.addProductDetails([product_details]);
+    productsToAttributes &&
+      (await ProductQuery.addProductToAttributes([productsToAttributes]));
+    inventory && (await InventoryQuery.addInventory([inventory]));
 
-		if (variants) {
-			const response = await ProductQuery.addProducts([product_variants]);
-			const startingInsertedIndex = response.insertId;
+    if (variants) {
+      const response = await ProductQuery.addProducts([product_variants]);
+      const startingInsertedIndex = response.insertId;
 
-			// product variations combination
-			const product_attributes_combination =
-				variants &&
-				variants.map((item, index) => [
-					index + startingInsertedIndex,
-					item.combinations
-				]);
+      // product variations combination
+      const product_attributes_combination =
+        variants &&
+        variants.map((item, index) => [
+          index + startingInsertedIndex,
+          item.combinations,
+        ]);
 
-			// variants inventory data
-			const variants_Inventory =
-				variants &&
-				variants.map((item, index) =>
-					Object.values(
-						ProductInventoryModel(
-							item,
-							index + startingInsertedIndex,
-							inserted_at,
-							updated_at
-						)
-					)
-				);
+      // variants inventory data
+      const variants_Inventory =
+        variants &&
+        variants.map((item, index) =>
+          Object.values(
+            ProductInventoryModel(
+              item,
+              index + startingInsertedIndex,
+              inserted_at,
+              updated_at
+            )
+          )
+        );
 
-			await InventoryQuery.addInventory([variants_Inventory]);
-			await ProductQuery.addProductToVariants([product_attributes_combination]);
-		}
-		const jsonObject = {
-			massage: "success",
-			product: {
-				...req.body,
-				product_id: product_id,
-				inserted_at: inserted_at,
-				updated_at: updated_at
-			}
-		};
-		res.status(201).json(jsonObject);
-	} catch (error) {
-		console.log(error);
-		res.status(500).json({ massage: "Something Went Wrong" });
-	}
+      await InventoryQuery.addInventory([variants_Inventory]);
+      await ProductQuery.addProductToVariants([product_attributes_combination]);
+    }
+    const jsonObject = {
+      massage: "success",
+      product: {
+        ...req.body,
+        product_id: product_id,
+        inserted_at: inserted_at,
+        updated_at: updated_at,
+      },
+    };
+    res.status(201).json(jsonObject);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ massage: "Something Went Wrong" });
+  }
 });
 
 module.exports = routes;
