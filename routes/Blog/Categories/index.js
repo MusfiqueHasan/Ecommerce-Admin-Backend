a9fb93c454222736f5635ec4fd51d4992341cd4b
Rat <ratulbhowmick65@gmail.com>
2021-10-06 21:43:39 +0600

    Category update

--- routes/Blog/Categories/index.js
+++ routes/Blog/Categories/index.js
@@ -5,101 +5,111 @@ const { NewCategoryModel } = require("../../../Modles/Category");
 const BlogQuerry = require("../../../Querry/BlogQuerry/CategoryQuerry");
 const { getTimeStamp } = require("../../../Utils/Utils");
 
-routes.get("/categories" ,async (req,res) => {
-    try {
-        const response = await BlogQuerry.getAllCategories();
-        const jsonObject = {
-            message : "success",
-            results : response
-        }
-        res.status(HTTPStatus.OK).json(jsonObject);
-    } catch (error) {
-        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({message : "INTERNAL SERVER ERROR"})
-    }
-})
+routes.get("/categories", async (req, res) => {
+  try {
+    const response = await BlogQuerry.getAllCategories();
+    const jsonObject = {
+      massage: "success",
+      results: response,
+    };
+    res.status(HTTPStatus.OK).json(jsonObject);
+  } catch (error) {
+    res
+      .status(HTTPStatus.INTERNAL_SERVER_ERROR)
+      .json({ massage: "INTERNAL SERVER ERROR" });
+  }
+});
 
-routes.post("/categories" ,async(req,res) => {
+routes.post("/categories", async (req, res) => {
+  const { name, description, parent_id } = req.body.categoryData;
 
-    const {
-        name ,
-        description,
-        parent_id
-    } = req.body.categoryData;
+  const parent_category = parent_id ? parent_id : null;
+  const category_name = name;
+  const updated_at = getTimeStamp();
+  const inserted_at = getTimeStamp();
 
-    const parent_category  = parent_id ? parent_id : null;
-    const category_name = name;
-    const updated_at = getTimeStamp();
-    const inserted_at = getTimeStamp();
-    
-    if(!description){
-        description = "";
-    }
+  if (!description) {
+    description = "";
+  }
 
-    const newCategoryArray = [
-        category_name,
-        parent_category,
-        updated_at,
-        inserted_at,
-        description
-    ]
-    const inputArray = [
-        newCategoryArray
-    ];
+  const newCategoryArray = [
+    category_name,
+    parent_category,
+    updated_at,
+    inserted_at,
+    description,
+  ];
+  const inputArray = [newCategoryArray];
 
-    try {
-        const response = await BlogQuerry.createNewCategory([inputArray]);
-        res.status(HTTPStatus.OK).json({message : "success"});
-    } catch (error) {
-        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({message : "INTERNAL SERVER ERROR"});
-    }
-})
+  try {
+    const response = await BlogQuerry.createNewCategory([inputArray]);
+    const jsonData = {
+      category_id: response.insertId,
+      name: category_name,
+      description: description,
+      parent_id: parent_category,
+    };
+    res.status(HTTPStatus.OK).json(jsonData);
+  } catch (error) {
+    res
+      .status(HTTPStatus.INTERNAL_SERVER_ERROR)
+      .json({ massage: "INTERNAL SERVER ERROR" });
+  }
+});
 
-routes.patch("/categories/:id",async(req,res) => {
-    const {id} = req.params;
-    const {
-        name ,
-        parent_id
-    } = req.body.categoryData;
+routes.patch("/categories/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, parent_id } = req.body.categoryData;
 
-    const parent_category  = parent_id ? parent_id : null;
-    const category_name = name;
-    const updated_at = getTimeStamp();
-    const inserted_at = getTimeStamp();
+  const parent_category = parent_id ? parent_id : null;
+  const category_name = name;
+  const updated_at = getTimeStamp();
+  const inserted_at = getTimeStamp();
+  const category_id = id;
 
-    const description = req.body.categoryData.description || ""; 
-    if(!category_id){
-        return res.status(HTTPStatus.BAD_REQUEST).json({message : "category id not found"});
-    }
+  const description = req.body.categoryData.description || "";
+  if (!category_id) {
+    return res
+      .status(HTTPStatus.BAD_REQUEST)
+      .json({ massage: "category id not found" });
+  }
 
-    const newCategoryArray = [
-        category_name,
-        parent_category,
-        updated_at,
-        description,
-        category_id
-    ]
-    try {
-        const response = await BlogQuerry.updateCategory(newCategoryArray);
-        res.status(HTTPStatus.OK).json({message : "success"});
-    } catch (error) {
-        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({message : "INTERNAL SERVER ERROR"});
-    }
-})
+  const newCategoryArray = [
+    category_name,
+    parent_category,
+    updated_at,
+    description,
+    category_id,
+  ];
+  try {
+    const response = await BlogQuerry.updateCategory(newCategoryArray);
 
-routes.delete("/categories/:id" , async(req,res) => {
-    const {id} = req.params;
+    res.status(HTTPStatus.OK).json({ status: "success" });
+  } catch (error) {
+    res
+      .status(HTTPStatus.INTERNAL_SERVER_ERROR)
+      .json({ massage: "INTERNAL SERVER ERROR" });
+  }
+});
 
-    const category_id = id ;
-    
-    if(!category_id){
-        return res.status(HTTPStatus.BAD_REQUEST).json({message : "category id not found"});
-    }
+routes.delete("/categories/:id", async (req, res) => {
+  const { id } = req.params;
 
-    try {
-        const response = await BlogQuerry.deleteCategory(category_id);
-        res.status(HTTPStatus.OK).json({message : "success"});
-    } catch (error) {
-        res.status(HTTPStatus.INTERNAL_SERVER_ERROR).json({message : "INTERNAL SERVER ERROR"});
-    }
-})
+  const category_id = id;
+
+  if (!category_id) {
+    return res
+      .status(HTTPStatus.BAD_REQUEST)
+      .json({ massage: "category id not found" });
+  }
+
+  try {
+    const response = await BlogQuerry.deleteCategory(category_id);
+    res.status(HTTPStatus.OK).json({ massage: "success" });
+  } catch (error) {
+    res
+      .status(HTTPStatus.INTERNAL_SERVER_ERROR)
+      .json({ massage: "INTERNAL SERVER ERROR" });
+  }
+});
 module.exports = routes;
